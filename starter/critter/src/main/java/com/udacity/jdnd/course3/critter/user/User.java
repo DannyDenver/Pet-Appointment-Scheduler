e68e6ea2473a2fd3e561e1105f979345d075ffa7
DannyDenver <djt09d@fulbrightmail.org>
2020-07-29 07:35:51 -0600

    change generationtype strategy for entity ids to identity

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/User.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/User.java
@@ -3,13 +3,14 @@ package com.udacity.jdnd.course3.critter.user;
 import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Nationalized
